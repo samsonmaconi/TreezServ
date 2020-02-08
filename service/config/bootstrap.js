01c2b4c759f36fae7d5caefef5db1e3cd6fa2413
Samson Maconi <git@samsonmaconi.com>
2020-02-07 23:13:14 -0800

    Customer and Inventory Data Bootstrapping

--- service/config/bootstrap.js
+++ service/config/bootstrap.js
@@ -11,20 +11,20 @@
 
 module.exports.bootstrap = async function() {
 
-  // By convention, this is a good place to set up fake data during development.
-  //
-  // For example:
-  // ```
-  // // Set up fake development data (or if we already have some, avast)
-  // if (await User.count() > 0) {
-  //   return;
-  // }
-  //
-  // await User.createEach([
-  //   { emailAddress: 'ry@example.com', fullName: 'Ryan Dahl', },
-  //   { emailAddress: 'rachael@example.com', fullName: 'Rachael Shaw', },
-  //   // etc.
-  // ]);
-  // ```
+  if (await Customer.count() === 0 ){
+    await Customer.createEach([
+      {id: 'sam@samsonmaconi.com', firstName: 'Samson', lastName: 'Maconi'}
+    ])
+  }
+
+  if (await Inventory.count() === 0 ){
+    await Inventory.createEach([
+      {name: 'Aquafina', description: 'Bottled Watter', price: '5', quantityAvailable: '20'},
+      {name: 'Bread', description: 'Plain Bread', price: '10', quantityAvailable: '6'},
+      {name: 'Milk', description: 'Almond Milk', price: '13.5', quantityAvailable: '17'},
+      {name: 'Plantains', price: '14.0', quantityAvailable: '5'},
+      {name: 'Mangoes', description: 'Awesome fruit', price: '8.45', quantityAvailable: '25'},
+    ])
+  }
 
 };
