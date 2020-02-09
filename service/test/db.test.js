242a3de80d6e451617a665e092917488732996e4
Samson Maconi <git@samsonmaconi.com>
2020-02-09 08:12:28 -0800

    Chores

--- service/test/db.test.js
+++ service/test/db.test.js
@@ -1,26 +1,28 @@
 const assert = require('assert');
 
-describe('DB TESTS', ()=> {
-  const name = 'Test Product 1'
-  const description = 'Mocha Test Product'
-  const price = 500
-  const quantityAvailable = 30
+describe('DB TESTS', () => {
+  const testProduct1 = {
+    name: 'Test Product 1',
+    description: 'Mocha Test Product',
+    price: 500,
+    quantityAvailable: 50
+  }
   let id = null
 
-  it('Test Insert and Fetching - Product Name should be `Test Product 1`', async ()=> {
-    let product = await Inventory.create({name, description, price, quantityAvailable}).fetch()
+  it('Test Insert and Fetching - Product Name should be `Test Product 1`', async () => {
+    let product = await Inventory.create(testProduct1).fetch()
     id = product.id
     product = null
     product = await Inventory.findOne(id)
-    assert.equal(product.name, name);
+    assert.equal(product.name, testProduct1.name);
   });
 
-  it('Test Update (Adjusting Stock Count) - Quantity should be 25', async ()=> {
-    let product = await Inventory.update(id).set({quantityAvailable: quantityAvailable-5}).fetch()
-    assert.equal(product[0].quantityAvailable, quantityAvailable - 5);
+  it('Test Update (Adjusting Stock Count) - Quantity should be 25', async () => {
+    let product = await Inventory.update(id).set({ quantityAvailable: testProduct1.quantityAvailable - 5 }).fetch()
+    assert.equal(product[0].quantityAvailable, testProduct1.quantityAvailable - 5);
   });
 
-  it('Test Deletion - Product should be null', async ()=> {
+  it('Test Deletion - Product should be null', async () => {
     let product = await Inventory.destroy(id).fetch()
     product = null
     product = await Inventory.findOne(id)
